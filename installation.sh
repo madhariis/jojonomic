c214ff22b24779bdb74fd27788aa1db57b72f55b
madhariis <madhariis49@gmail.com>
2021-02-28 17:48:14 +0700

    add installation sh and env example

---
+++ installation.sh
@@ -0,0 +1,28 @@
+## Build Guide
+
+###Import the postman collection
+- Link postman collection
+
+```
+
+https://www.getpostman.com/collections/abd5c85c57437c881cdf
+
+```
+
+### Requirement
+
+- Go 1.15
+- MongoDB 4.4.4
+
+### Build
+
+- clone repository
+
+```
+
+go run main.go
+
+```
+
+
+---
